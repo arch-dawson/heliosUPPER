9db6391537f271ef911b77a847890dd625070990
blackberrypi <ryanjcutter@gmail.com>
2016-05-12 20:32:55 +0000

    Modified serv.py

--- flight_UPPER/inpt/inpt.py
+++ flight_UPPER/inpt/inpt.py
@@ -1,2 +1,16 @@
-def main(input, empty):
-	print("inside main func in inpt")
+import time
+
+#def main(inputQ,empty):
+print("inside main func in inpt")
+
+def test(inputQ,blank):
+	print('woo')
+
+
+#inputQ.put(["IN"])
+#while True:
+	#time.sleep(2)
+	#if send.inWaiting():
+		#one=send.read()
+		#two=send.waitByte()
+#return 0
