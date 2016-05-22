45038d5ff38954a484024db22afc672557b48ecc
dbeattyspace <dabe5458@colorado.edu>
2016-05-22 15:11:08 -0600

    Update capt.py
    Combining capt.py and capture.py for simplicity.

--- flight_UPPER/capt/capt.py
+++ flight_UPPER/capt/capt.py
@@ -1,23 +1,57 @@
+# *********************************************************#
+#   COSGC Presents										   #
+#      __  __________    ________  _____   __    __        #
+#     / / / / ____/ /   /  _/ __ \/ ___/   | |  / /        #
+#    / /_/ / __/ / /    / // / / /\__ \    | | / /         #
+#   / __  / /___/ /____/ // /_/ /___/ /    | |/ /          #
+#  /_/ /_/_____/_____/___/\____//____/     |___/           #  
+#                                                          #
+#   													   #
+#  Copyright (c) 2016 University of Colorado Boulder	   #
+#  COSGC HASP Helios V Team							       #
+# *********************************************************#
+
 import time
 import queue
-from capt import capture
+# from capt import capture
+import threading
+import os.path as path
+import subprocess
+
+capture_code = "/home/pi/heliosUPPER/flight_UPPER/capt/a.out"
+
+class Cameras:
+	
+	def __init__(self,toLowerQ):
+		self.toLowerQ = toLowerQ
+		subprocess.call([capture_code, "-t"])
+		#logging.info("Initialized capture code")
+		#print("successfully ran test")
+		#self.adcs_path = "/dev/v4l/by-id/..."		#Paths need to be completed
+		self.sci_path = "/dev/v4l/by-id/usb-The_Imaging_Source_Europe_GmbH_DMK_23U274_17510268-video-index0"		
+		self.lock = threading.Lock()
+	
+	def science(self):
+		# Captures a science and an adcs image, naming them with the "Camera_hr-min-sec.png" convention 
+		#t = time.gmtime()
+		t = str(time.time())
+		#adcs_name = "/home/pi/hasp_temp/flight/images/ADCS_" + str(t[3]) + "-" + str(t[4]) + "-" + str(t[5]) + ".png"
+		#sci_name = "/home/pi/heliosUPPER/flight_UPPER/capt/images/SCI_" +  str(random.randint(0, 2**32)) + ".png"
+		sci_name = "/home/pi/heliosUPPER/flight_UPPER/capt/images/SCI_" + t + ".png" 
+		with self.lock:
+			subprocess.call([capture_code, "-d", self.sci_path, "-o", sci_name])
+			#subprocess.call([capture_code, "-d", self.adcs_path, "-o", adcs_name])
+		#logging.info("Captured " + sci_name)
+		threading.Timer(10,science(self)).start()
+		self.toLowerQ.put("Took Science picture with timestamp: " + t)
+		print("Took science picture with timestamp: " + t)
+		return sci_name
 
 def main(toLowerQ):
 	print("Initializing Science Camera")
 	camera = Cameras(toLowerQ)
 	# Initialize initial photo capture rate
-	rate = 3;
-	#while True:
-		#if (capt_cmd.empty() == false):
-			#cmd = capt_cmd.get()
-			#if type(cmd) is bytes:
-				#packet = hex(int.from_bytes(cmd, byteorder = 'big'))
-            	## This is where the command is parsed and the rate is changed if necessary 	
+	rate = 3; #PASS RATE TO CAMERAS AS WELL
 
-        # Take photo 
 	cameras.science()
-
-		#downlink.put(["AD", "CP", "Science Image Taken"])
-		# Sleep for time equal to rate
-		#time.sleep(rate)
 
